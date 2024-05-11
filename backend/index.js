65f2d3bea99597199fb198c2cbabd800802a083a
Teerath Agarwal <teerath173@gmail.com>
2024-05-12 01:11:17 +0530

    Backend: cors middleware

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,13 @@ const app = express()
 dotenv.config()
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,');
+    next();
+});
+
 app.get('/', (req,res) => {
     res.status(200).send('Backend running successfully')
 })
