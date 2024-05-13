96f66eff3f762d252b5821287dbf441fa86fb394
Teerath Agarwal <teerath173@gmail.com>
2024-05-13 17:43:30 +0530

    Backend: Routes - put by user

--- backend/taskRoutes/routes.js
+++ backend/taskRoutes/routes.js
@@ -1,4 +1,4 @@
-import connection from "../database/config";
+import connection from "../database/config.js";
 
 function getByUsr(req, res) {
     connection.query(
@@ -6,13 +6,35 @@ function getByUsr(req, res) {
         (err, results) => {
         if (err){
             res.status(403).send({
-                verdict: 'Error fetching tasks',
+                verdict: `Error fetching tasks: ${err}`,
                 success: false,
             });
             return;
         }
         res.status(200).send({
             result: results,
+            success: true,
+        });
+    });
+}
+
+function putByUsr(req, res) {
+    const { user, title = "", task, completed = false, deadline = "1970-01-01", priority = 0  }  = req.body;
+
+    connection.query(`
+        INSERT INTO Tasks (user, title, task, completed, deadline, priority)
+        VALUES (
+            '${user}', '${title}', '${task}', '${completed}', '${deadline}', '${priority}');`, 
+        (err, result) => {
+        if (err){
+            res.status(403).send({
+                verdict: `Error creating new task: ${err}`,
+                success: false,
+            });
+            return;
+        }
+        res.status(200).send({
+            verdict: result,
             success: true,
         });
     });
